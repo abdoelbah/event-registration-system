a9ac6b987fe9ce86e96cc831775ccbc79e3c8e73
abdoelbah <aeempaire@gmail.com>
2025-05-26 17:41:40 +0200

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Button } from "@material-tailwind/react";
+
+function App() {
+
+  return (
+    <>
+<h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Button>Button</Button>
+    </>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const app = express()
+const Router = require('./router/router')
+
+const port = process.env.PORT || 3000
+// const mongoUri = process.env.MONGO_URL_DEV
+const mongoUri = process.env.MONGO_URL_PROD
+console.log(mongoUri)
+console.log(port)
+
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB')
+}).catch((err) => {
+    console.log(err)
+})
+
+// Set up CORS to allow requests from all origins
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(Router)
+app.listen(port, () => {
+    console.log(`App listening on port ${port}!`)
+})
